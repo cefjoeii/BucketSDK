01d3da1b8f738d8c2de4c3736f31b83ffdf43c78
Ryan <ryan@buckettechnologies.com>
2018-04-06 15:06:55 -0400

    Added in the example

---
+++ Example/Pods/Target Support Files/BucketSDK/BucketSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BucketSDK : NSObject
+@end
+@implementation PodsDummy_BucketSDK
+@end
---
+++ Example/Pods/Target Support Files/Pods-BucketSDK_Tests/Pods-BucketSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BucketSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_BucketSDK_Tests
+@end
