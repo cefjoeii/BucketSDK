367e6b2b782b3db8884e313f649061b3c6fbb9d7
Ceferino Jose II <ceferinojo@cloudstaff.com>
2018-07-23 15:59:39 +0800

    Updating unit test cases

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -7,6 +7,7 @@ class Tests: XCTestCase {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
+        Bucket.shared.environment = .development
     }
     
     override func tearDown() {
@@ -14,32 +15,99 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure() {
             // Put the code you want to measure the time of here.
+            
         }
     }
     
-    func testCreateTransaction() {
+    func testRegisterDevice() {
+        let expectation = XCTestExpectation(description: "Register the device with that terminalId.")
         
-        // Make sure the client id & client secret is set:  these are 
+        Bucket.shared.registerDevice(with: "1234") { (success, error) in
+            XCTAssertTrue(success, "For now, the register device function should work.")
+            XCTAssertNil(error)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 2)
+    }
+    
+    func testFetchBillDenominations() {
+        let expectationForUSD = XCTestExpectation(description: "Fetch the USD denominations.")
+        Bucket.shared.fetchBillDenominations(for: .usd) { (success, error) in
+            XCTAssertTrue(success, "USD denominations should be fetched.")
+            XCTAssertNil(error)
+            expectationForUSD.fulfill()
+        }
+        
+        wait(for: [expectationForUSD], timeout: 1)
+        
+        let expectationForSGD = XCTestExpectation(description: "Fetch the SGD denominations.")
+        Bucket.shared.fetchBillDenominations(for: .sgd) { (success, error) in
+            XCTAssertTrue(success, "SGD denominations should be fetched.")
+            XCTAssertNil(error)
+            expectationForSGD.fulfill()
+        }
+        
+        wait(for: [expectationForSGD], timeout: 1)
+    }
+    
+    func testCloseInterval() {
+        let expectation = XCTestExpectation(description: "Close the interval with the interval id of '20170101'.")
+        
+        Bucket.shared.close(intervalId: "20170101") { (response, success, error) in
+            XCTAssertFalse(success, "intervalId = '20170101' has been previously closed.")
+            XCTAssertNotNil(error)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    func testBucketAmount() {
+        var bucketAmount = Bucket.shared.bucketAmount(for: 1000)
+        XCTAssertEqual(bucketAmount, 0, "Bucket amount should be zero when the change due back is 1000.")
+        
+        bucketAmount = Bucket.shared.bucketAmount(for: 1234)
+        XCTAssertEqual(bucketAmount, 34, "Bucket amount should be 34 when the change due back 1234.")
+    }
+    
+    func testCreateTransaction() {
+        let expectation = XCTestExpectation(description: "Create a transaction.")
+        
+        // Make sure the retailer id, retailer secret, and terminal id are set.
         Bucket.Credentials.retailerId = "6644211a-c02a-4413-b307-04a11b16e6a4"
         Bucket.Credentials.retailerSecret = "9IlwMxfQLaOvC4R64GdX/xabpvAA4QBpqb1t8lJ7PTGeR4daLI/bxw=="
         Bucket.Credentials.terminalId = "qwerty1234"
         
-        let transaction = Bucket.Transaction(amount: 78)
-        transaction.create { (success, error) in
-            if success {
-                print(success)
-            } else if !error.isNil {
-                print(error!.localizedDescription)
+        let transaction = Bucket.Transaction(amount: 7843, clientTransactionId: "test")
+        transaction.create { (response, success, error) in
+            if (success) {
+                XCTAssertNotNil(response)
+                
+                if let response = response {
+                    XCTAssertEqual(response.amount, 7843)
+                    XCTAssertEqual(response.clientTransactionId, "test")
+                    XCTAssertNotEqual(response.customerCode, "")
+                    XCTAssertNotNil(response.qrCodeContent)
+                }
+                
+                XCTAssertTrue(success)
+                XCTAssertNil(error)
+            } else {
+                XCTAssertNil(response)
+                XCTAssertFalse(success)
+                XCTAssertNotNil(error)
             }
+            
+            expectation.fulfill()
         }
+        
+        wait(for: [expectation], timeout: 5)
     }
 }
