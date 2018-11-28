d96c552278d0017724ab9ad6985239f9c813ab1f
Ceferino Jose II <ceferinojo@cloudstaff.com>
2018-11-28 19:09:36 +0800

    Updating the unit test for the register terminal in Objective-C

--- Example/Tests/ObjectiveCTests.m
+++ Example/Tests/ObjectiveCTests.m
@@ -25,22 +25,18 @@
     [super tearDown];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
-
-- (void)testRegisterDevice {
-    XCTestExpectation *expectation =  [[XCTestExpectation alloc] initWithDescription:@"Register the device with that terminalId."];
+- (void)testRegisterTerminal {
+    XCTestExpectation *expectation =  [[XCTestExpectation alloc] init];
     
-//    [[Bucket shared] registerDeviceWith:@"1234" :^(BOOL success, NSError * _Nullable error) {
-//        XCTAssertTrue(success == YES, @"For now, the register device function should just work.");
-//        XCTAssertNil(error);
-//
-//        [expectation fulfill];
-//    }];
+    [[Bucket shared] registerTerminalWithRetailerCode:@"bckt-1" country:@"us" completion:^(BOOL success, NSError * _Nullable error) {
+        if (success) {
+            XCTAssertNil(error);
+        } else {
+            XCTAssertNotNil(error);
+        }
+        
+        [expectation fulfill];
+    }];
     
     [self waitForExpectations:[NSArray arrayWithObjects:expectation,nil] timeout:5];
 }
@@ -48,36 +44,36 @@
 - (void)testFetchBillDenominations {
     XCTestExpectation *expectationForUSD =  [[XCTestExpectation alloc] initWithDescription:@"Fetch the USD denominations."];
     
-//    [[Bucket shared] fetchBillDenominationsFor:BillDenominationUsd :^(BOOL success, NSError * _Nullable error) {
-//        XCTAssertTrue(success == YES, @"USD denominations should be fetched.");
-//        XCTAssertNil(error);
-//
-//        [expectationForUSD fulfill];
-//    }];
+    //    [[Bucket shared] fetchBillDenominationsFor:BillDenominationUsd :^(BOOL success, NSError * _Nullable error) {
+    //        XCTAssertTrue(success == YES, @"USD denominations should be fetched.");
+    //        XCTAssertNil(error);
+    //
+    //        [expectationForUSD fulfill];
+    //    }];
     
     [self waitForExpectations:[NSArray arrayWithObjects:expectationForUSD,nil] timeout:1];
     
     XCTestExpectation *expectationForSGD =  [[XCTestExpectation alloc] initWithDescription:@"Fetch the SGD denominations."];
     
-//    [[Bucket shared] fetchBillDenominationsFor:BillDenominationSgd :^(BOOL success, NSError * _Nullable error) {
-//        XCTAssertTrue(success == YES, @"SGD denominations should be fetched.");
-//        XCTAssertNil(error);
-//        
-//        [expectationForSGD fulfill];
-//    }];
+    //    [[Bucket shared] fetchBillDenominationsFor:BillDenominationSgd :^(BOOL success, NSError * _Nullable error) {
+    //        XCTAssertTrue(success == YES, @"SGD denominations should be fetched.");
+    //        XCTAssertNil(error);
+    //
+    //        [expectationForSGD fulfill];
+    //    }];
     
     [self waitForExpectations:[NSArray arrayWithObjects:expectationForSGD,nil] timeout:1];
 }
 
 - (void)testBucketAmount {
-//    long bucketAmount = [[Bucket shared] bucketAmountFor:1000];
-//    XCTAssertEqual(bucketAmount, 0, @"Bucket amount should be zero when the change due back is 1000.");
-//    
-//    bucketAmount = [[Bucket shared] bucketAmountFor:1234];
-//    XCTAssertEqual(bucketAmount, 34, @"Bucket amount should be 34 when the change due back is 1234.");
-//    
-//    bucketAmount = [[Bucket shared] bucketAmountForDecimal:1.0];
-//    XCTAssertEqual(bucketAmount, 100, @"Bucket amout should be 100, not 10, when the change due back is 1.0 or 1.00.");
+    //    long bucketAmount = [[Bucket shared] bucketAmountFor:1000];
+    //    XCTAssertEqual(bucketAmount, 0, @"Bucket amount should be zero when the change due back is 1000.");
+    //
+    //    bucketAmount = [[Bucket shared] bucketAmountFor:1234];
+    //    XCTAssertEqual(bucketAmount, 34, @"Bucket amount should be 34 when the change due back is 1234.");
+    //
+    //    bucketAmount = [[Bucket shared] bucketAmountForDecimal:1.0];
+    //    XCTAssertEqual(bucketAmount, 100, @"Bucket amout should be 100, not 10, when the change due back is 1.0 or 1.00.");
 }
 
 - (void)testCreateTransaction {
@@ -90,28 +86,28 @@
     
     long amount = 7834;
     
-//    Transaction *transaction = [[Transaction alloc] initWithAmount:amount clientTransactionId:@"test"];
-//    [transaction create :^(CreateTransactionResponse * _Nullable response, BOOL success, NSError * _Nullable error) {
-//        if (success == YES) {
-//            XCTAssertNotNil(response);
-//            
-//            if (response != NULL) {
-//                XCTAssertEqual(response.amount, amount);
-//                XCTAssertTrue([response.clientTransactionId isEqualToString:@"test"]);
-//                XCTAssertNotEqual(response.customerCode, @"");
-//                XCTAssertNotNil(response.qrCodeContent);
-//            }
-//            
-//            XCTAssertTrue(success == YES);
-//            XCTAssertNil(error);
-//        } else {
-//            XCTAssertNil(response);
-//            XCTAssertFalse(success == YES);
-//            XCTAssertNotNil(error);
-//        }
-//        
-//        [expectation fulfill];
-//    }];
+    //    Transaction *transaction = [[Transaction alloc] initWithAmount:amount clientTransactionId:@"test"];
+    //    [transaction create :^(CreateTransactionResponse * _Nullable response, BOOL success, NSError * _Nullable error) {
+    //        if (success == YES) {
+    //            XCTAssertNotNil(response);
+    //
+    //            if (response != NULL) {
+    //                XCTAssertEqual(response.amount, amount);
+    //                XCTAssertTrue([response.clientTransactionId isEqualToString:@"test"]);
+    //                XCTAssertNotEqual(response.customerCode, @"");
+    //                XCTAssertNotNil(response.qrCodeContent);
+    //            }
+    //
+    //            XCTAssertTrue(success == YES);
+    //            XCTAssertNil(error);
+    //        } else {
+    //            XCTAssertNil(response);
+    //            XCTAssertFalse(success == YES);
+    //            XCTAssertNotNil(error);
+    //        }
+    //
+    //        [expectation fulfill];
+    //    }];
     
     [self waitForExpectations:[NSArray arrayWithObjects:expectation,nil] timeout:5];
 }
