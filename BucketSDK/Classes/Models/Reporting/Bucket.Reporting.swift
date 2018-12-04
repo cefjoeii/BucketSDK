7e3b1b23fe6603c2fdd55bc665e06d706034dfaf
Ceferino Jose II <ceferinojo@cloudstaff.com>
2018-12-04 23:27:30 +0800

    Working on the pagination for the report function

--- BucketSDK/Classes/Models/Reporting/Bucket.Reporting.swift
+++ BucketSDK/Classes/Models/Reporting/Bucket.Reporting.swift
@@ -11,7 +11,7 @@ extension Bucket {
     /// Allows a POS integration developer to give a summed history of the transactions.
     @objc public func getReport(
         _ getReportRequest: GetReportRequest,
-        completion: @escaping ((_ success: Bool, _ response: GetReportsResponse?, _ error: Error?) -> Void)
+        completion: @escaping ((_ success: Bool, _ response: GetReportsResponse?, _ canPage: Bool, _ error: Error?) -> Void)
         ) {
         
         // Some of these pitfalls should never occur unless the SDK is manually modified.
@@ -19,45 +19,45 @@ extension Bucket {
         case 1:
             if !(getReportRequest.range["day"] is String && (getReportRequest.range["day"] as! String).isValidDayDate) {
                 // Return if the day is not a String and its format is invalid.
-                completion(false, nil, BucketErrorResponse.invalidDateRange)
+                completion(false, nil, true, BucketErrorResponse.invalidDateRange)
                 return
             }
         case 2:
             if getReportRequest.range["start"] is String && getReportRequest.range["end"] is String {
                 if (getReportRequest.range["start"] as! String).isNotValidStartEndDate || (getReportRequest.range["end"] as! String).isNotValidStartEndDate {
                     // Return if neither start nor end is a valid date.
-                    completion(false, nil, BucketErrorResponse.invalidDateRange)
+                    completion(false, nil, true, BucketErrorResponse.invalidDateRange)
                     return
                 }
             } else if !(getReportRequest.range["start"] is Int && getReportRequest.range["end"] is Int) {
                 // Return if the start and end is neither a pair of Ints nor a pair of Strings above.
-                completion(false, nil, BucketErrorResponse.invalidDateRange)
+                completion(false, nil, true, BucketErrorResponse.invalidDateRange)
                 return
             }
         default:
             // Return if the range dictionary count is invalid.
-            completion(false, nil, BucketErrorResponse.invalidDateRange)
+            completion(false, nil, true, BucketErrorResponse.invalidDateRange)
             return
         }
         
         // Return and tell the developer that the employee code is required.
         if Terminal.requireEmployeeCode && getReportRequest.employeeCode.isNil {
-            completion(false, nil, BucketErrorResponse.invalidEmployeeCode)
+            completion(false, nil, true, BucketErrorResponse.invalidEmployeeCode)
             return
         }
         
         guard let retailerCode = Credentials.retailerCode, let terminalSecret = Credentials.terminalSecret else {
-            completion(false, nil, BucketErrorResponse.invalidRetailer)
+            completion(false, nil, true, BucketErrorResponse.invalidRetailer)
             return
         }
         
         guard let terminalCode = Credentials.terminalCode else {
-            completion(false, nil, BucketErrorResponse.noTerminalId)
+            completion(false, nil, true, BucketErrorResponse.noTerminalId)
             return
         }
         
         guard let country = Credentials.country else {
-            completion(false, nil, BucketErrorResponse.invalidCountryCode)
+            completion(false, nil, true, BucketErrorResponse.invalidCountryCode)
             return
         }
         
@@ -77,20 +77,26 @@ extension Bucket {
         request.setBody(getReportRequest.body)
         
         URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else { completion(false, nil, error); return }
+            guard let data = data else { completion(false, nil, true, error); return }
             
             if response.isSuccess {
                 do {
                     // Map the json response to the model class.
                     let response = try JSONDecoder().decode(GetReportsResponse.self, from: data)
                     
-                    completion(true, response, nil)
+                    var canPage = true
+                    
+                    if let count = response.transactions?.count {
+                        canPage = (count >= getReportRequest.limit)
+                    }
+                    
+                    completion(true, response, canPage, nil)
                 } catch let error {
-                    completion(false, nil, error)
+                    completion(false, nil, true, error)
                 }
             } else {
                 let bucketErrorResponse = try? JSONDecoder().decode(BucketErrorResponse.self, from: data)
-                completion(false, nil, bucketErrorResponse?.asError(response?.code) ?? BucketErrorResponse.unknown)
+                completion(false, nil, bucketErrorResponse?.errorCode != "EmptyReport", bucketErrorResponse?.asError(response?.code) ?? BucketErrorResponse.unknown)
             }
             }.resume()
     }
